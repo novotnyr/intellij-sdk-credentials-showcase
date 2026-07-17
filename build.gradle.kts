666e6efc3f99e009b5c134184a5c4f762ddbb4b7
Robert Novotny <robert.novotny@jetbrains.com>
2026-07-17 14:34:36 +0200

    Initial import

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+
+plugins {
+    id("org.jetbrains.kotlin.jvm")
+    id("org.jetbrains.intellij.platform")
+}
+
+dependencies {
+    intellijPlatform {
+        intellijIdea("2025.3.5")
+    }
+}

92b97078073ac72d83305fa924376584b6b8eb36
Robert Novotny <robert.novotny@jetbrains.com>
2026-07-22 16:26:51 +0200

    Rename methods

--- src/main/kotlin/org/intellij/sdk/credentials/ApiKeyService.kt
+++ src/main/kotlin/org/intellij/sdk/credentials/ApiKeyService.kt
@@ -1,17 +1,14 @@
 package org.intellij.sdk.credentials
 
 import com.intellij.credentialStore.CredentialAttributes
-import com.intellij.credentialStore.OneTimeString
+import com.intellij.credentialStore.Credentials
 import com.intellij.credentialStore.generateServiceName
 import com.intellij.ide.passwordSafe.PasswordSafe
-import com.intellij.openapi.application.EDT
 import com.intellij.openapi.components.Service
-import com.intellij.util.Ephemeral
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import kotlin.coroutines.CoroutineContext
 
 private const val SERVICE_NAME = "Credentials Showcase"
 
@@ -25,13 +22,13 @@ class ApiKeyService(val cs: CoroutineScope) {
 
     private val credentialAttributes = CredentialAttributes(serviceName, "API_KEY")
 
-    fun set(apiKey: ApiKey) {
+    fun save(apiKey: ApiKey) {
         cs.launch {
-            passwordSafe.setPassword(credentialAttributes, apiKey)
+            passwordSafe[credentialAttributes] = Credentials(null, apiKey)
         }
     }
 
-    suspend fun get(): ApiKey? = withContext(Dispatchers.IO) {
-        passwordSafe.getPassword(credentialAttributes)
+    suspend fun find(): ApiKey? = withContext(Dispatchers.IO) {
+        passwordSafe[credentialAttributes]?.password?.toString()
     }
 }
